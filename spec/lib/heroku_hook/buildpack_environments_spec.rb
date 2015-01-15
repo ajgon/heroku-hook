2bd1d75ec19402ff240194d7d7facb19a65de814
Igor Rzegocki <ajgon@irgon.com>
2015-01-15 16:29:22 +0100

    Fixed broken test

--- spec/lib/heroku_hook/buildpack_environments_spec.rb
+++ spec/lib/heroku_hook/buildpack_environments_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe 'Buildpack environments' do
 
     expect(HerokuHook::BuildpackEnvironments.php).to eq(
       'HOME' => '/opt/apps/bare/_app',
-      'LD_LIBRARY_PATH' => '/opt/apps/bare/_app/.heroku/php/lib'
+      'LD_LIBRARY_PATH' => '/opt/apps/bare/_app/.heroku/php/lib',
+      'PHP_INI_SCAN_DIR' => '/opt/apps/bare/_app/.heroku/php/etc/php/conf.d'
     )
   end
 end
